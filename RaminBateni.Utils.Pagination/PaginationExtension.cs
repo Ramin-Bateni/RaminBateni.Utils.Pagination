49ad483e1fcb98b9f6bbf2973e794948b12633ba
Ramin-Bateni <ram_20designer@yahoo.com>
2022-07-25 18:03:06 +0430

    Add project files.

---
+++ RaminBateni.Utils.Pagination/PaginationExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RaminBateni.Utils.Pagination.Models;
+
+namespace RaminBateni.Utils.Pagination
+{
+    public static class PaginationExtension
+    {
+        public static async Task<PagedResult<T>> AsPagedToListAsync<T>(
+            this IQueryable<T> query,
+            int page, int pageSize)
+            where T : class
+        {
+            var result = new PagedResult<T>
+            {
+                CurrentPage = page,
+                PageSize = pageSize,
+                RowCount = await query.CountAsync()
+            };
+
+            var pageCount = (double)result.RowCount / pageSize;
+            result.PageCount = (int)Math.Ceiling(pageCount);
+
+            var skip = (page - 1) * pageSize;
+            result.SetItems(await query.Skip(skip).Take(pageSize).ToListAsync());
+
+            return result;
+        }
+    }
+}
