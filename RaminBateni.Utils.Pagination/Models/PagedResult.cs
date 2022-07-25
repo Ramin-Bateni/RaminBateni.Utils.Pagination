c671de4d625d18501d178c1f387bf8c7e49c401f
Ramin-Bateni <ram_20designer@yahoo.com>
2022-07-25 18:03:06 +0430

    Add project files.

---
+++ RaminBateni.Utils.Pagination/Models/PagedResult.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RaminBateni.Utils.Pagination.Models
+{
+    public class PagedResult<T>
+    {
+        public int CurrentPage { get; set; }
+
+        public int PageCount { get; set; }
+
+        public int PageSize { get; set; }
+
+        public int RowCount { get; set; }
+
+        public int FirstRowOnPage => (CurrentPage - 1) * PageSize + 1;
+
+        public int LastRowOnPage => Math.Min(CurrentPage * PageSize, RowCount);
+
+        public List<T> Items { get; private set; }
+
+        public PagedResult()
+        {
+            Items = new List<T>();
+        }
+
+        public void SetItems(List<T> items)
+        {
+            Items = items;
+        }
+    }
+}
